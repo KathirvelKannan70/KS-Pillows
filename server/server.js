6920a04085abe29434eaa36e76a7cb0799d7b7ef
DharshiniManikandan-1203 <dharshinim1203@gmail.com>
2026-02-23 22:08:59 +0530

    Added Kspillows Domain

--- server/server.js
+++ server/server.js
@@ -31,6 +31,7 @@ app.use(
       "http://localhost:5173",
       "https://ks-pillows-nd5s.vercel.app",
       "https://ks-pillows-nd5s.vercel.app/",
+      "https://kspillows.in",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
