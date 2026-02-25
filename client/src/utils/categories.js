41e3d62fa0af1d2b57f575659994cbd15e8c4ca2
DharshiniManikandan-1203 <dharshinim1203@gmail.com>
2026-02-25 22:41:28 +0530

    Added Categories Photo

--- client/src/utils/categories.js
+++ client/src/utils/categories.js
@@ -4,30 +4,30 @@ export const CATEGORIES = [
         name: "Kapok Pillow",
         slug: "kapok-pillow",
         image:
-            "https://drive.google.com/file/d/1qbkPgSumFipNj5FP0mjRUgdhunRUkrh5/view?usp=sharing",
+            "https://lh3.googleusercontent.com/d/1qbkPgSumFipNj5FP0mjRUgdhunRUkrh5",
     },
     {
         name: "Recron Pillow",
         slug: "recron-pillow",
         image:
-            "https://drive.google.com/file/d/1h8DVJ7OAXds760VioIqSPJArpCoZCZoc/view?usp=drive_link",
+            "https://lh3.googleusercontent.com/d/1h8DVJ7OAXds760VioIqSPJArpCoZCZoc",
     },
     {
         name: "Kapok Mattresses",
         slug: "kapok-mattresses",
         image:
-            "https://drive.google.com/file/d/1g3Jlvfh_sSzKnJdjqQi3WSYaviRh-kgk/view?usp=sharing",
+            "https://lh3.googleusercontent.com/d/1g3Jlvfh_sSzKnJdjqQi3WSYaviRh-kgk",
     },
     {
         name: "Travel Quilt Bed",
         slug: "travel-quilt-bed",
         image:
-            "https://drive.google.com/file/d/1yw6FlbGtXNFfTzewe2Jr3B6Z4cPb8vr9/view?usp=drive_link",
+            "https://lh3.googleusercontent.com/d/1yw6FlbGtXNFfTzewe2Jr3B6Z4cPb8vr9",
     },
     {
         name: "Korai Pai Bed",
         slug: "korai-pai-bed",
         image:
-            "https://drive.google.com/file/d/1UAsrv2Rb8EbKGGm7WiGyAvEu7BATHcSx/view?usp=drive_link",
+            "https://lh3.googleusercontent.com/d/1UAsrv2Rb8EbKGGm7WiGyAvEu7BATHcSx",
     },
 ];
